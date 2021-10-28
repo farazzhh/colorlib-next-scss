aa8d3590281fcba07ab552e39e5042190127ad66
farazzhh <havaee.faraz@gmail.com>
2021-10-28 22:54:15 +0330

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import '../styles/globals.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
